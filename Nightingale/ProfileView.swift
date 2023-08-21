346e1c273e27622f53bc3c07ad63a43260c25b4f
Shreyas <shreyassajith13@gmail.com>
2023-08-21 16:49:39 -0400

    Profile View

--- Nightingale/ProfileView.swift
+++ Nightingale/ProfileView.swift
@@ -8,10 +8,39 @@
 import SwiftUI
 
 struct ProfileView: View {
-    var body: some View {
-        Text("Profile View")
+
+        @State var firstname: String = ""
+        @State var lastname: String = ""
+        @State var username: String = ""
+        @State var birthdate = Date()
+        
+        var body: some View {
+            NavigationView{
+                
+                
+                Form {
+                    Section(header: Text("Personal Information")){
+                        TextField("First name", text: $firstname)
+                        
+                            .autocapitalization(.none)
+                        SecureField("Last name", text: $lastname)
+                            .autocapitalization(.none)
+                        SecureField("Username", text: $username)
+                            .autocapitalization(.none)
+                        DatePicker("Birthdate:", selection: $birthdate, displayedComponents: .date)}
+                    Section(header: Text("Avatar")){
+                        
+                            
+                            
+                        }
+                    
+                }
+                .navigationTitle("Account")
+                
+            }
+        }
     }
-}
+
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
